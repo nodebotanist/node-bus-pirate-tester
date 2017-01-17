bac1318ca22cc7f15f877a8dbf516cf3ad5972c9
Kas Perch <the@nodebotani.st>
2017-01-17 01:36:06 -0600

    init server

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+
+const Mocha = require('mocha')
+
+let mocha = new Mocha()
+
+// reply to request with "Hello World!"
+app.get('/', function(req, res) {
+    mocha.addFile('./bus-pirate/test/all.js')
+    mocha.run((failures) => {
+        if (failures) {
+            res.sendStatus(500)
+            res.end(failures)
+        } else {
+            res.sendStatus(200)
+            res.end()
+        }
+    })
+});
+
+//start a server on port 80 and log its start to our console
+var server = app.listen(80, function() {
+
+    var port = server.address().port;
+    console.log('Example app listening on port ', port);
+
+});
