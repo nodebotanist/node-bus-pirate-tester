acdfa99a5e8755357f31bc91078e3ca3de6ad813
Kas Perch <the@nodebotani.st>
2017-01-27 17:50:13 -0600

    Added tests

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ let mocha = new Mocha()
 // reply to request with "Hello World!"
 app.get('/', function(req, res) {
     mocha.addFile('./test/all.js')
+    mocha.addFile('./hardware-tests/general.js')
     mocha.run((failures) => {
         if (failures) {
             res.sendStatus(500)
