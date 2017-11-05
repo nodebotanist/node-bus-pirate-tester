a1237d36d86c9d747c44aa8811c63d26aa712cf5
Kas Perch <the@nodebotani.st>
2017-11-05 15:20:05 -0600

    More UART stuff

--- libs/UART.c
+++ libs/UART.c
@@ -4,6 +4,9 @@ void uart_init(void){
   //set baud rate
   UBBRH = (BAUD_RATE>>8);
   UBRRL = BAUD_RATE;
+  // enable RX/TX pins
   UCSRB |= (1<<TXEN)|(1<<RXEN);
+  // set transmission size to 8 bits (1 byte)
+  UCSRC |= (1<<URSEL)|(1<<UCSZ0)|(1<<USCZ1)
   return;
 }
