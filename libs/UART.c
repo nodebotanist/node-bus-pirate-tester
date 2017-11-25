a78fe0a569708c977d21ec7f888badf356c8b582
Kas Perch <the@nodebotani.st>
2017-11-24 18:10:42 -0600

    Starting UART library

--- libs/UART.c
+++ libs/UART.c
@@ -2,11 +2,46 @@
 
 void uart_init(void){
   //set baud rate
-  UBBRH = (BAUD_RATE>>8);
+  UBBRH = (BAUD_RATE >> 8);
   UBRRL = BAUD_RATE;
   // enable RX/TX pins
   UCSRB |= (1<<TXEN)|(1<<RXEN);
   // set transmission size to 8 bits (1 byte)
-  UCSRC |= (1<<URSEL)|(1<<UCSZ0)|(1<<USCZ1)
+  UCSRC |= (1<<URSEL)|(1<<UCSZ0)|(1<<USCZ1);
   return;
+}
+
+void send_byte(uint8_t data){
+  loop_until_bit_is_set(UCSR0A, UDRE0);
+  UDR0 = data;  
+}
+
+uint8_t recieve_byte(void){
+  loop_until_bit_is_set(UCSR0A, RXC0);
+  return UDR0
+}
+
+void print_string(const char() string_to_send){
+  uint8_t i = 0;
+  while(string_to_send[i]){
+    send_byte(string_to_send[i]);
+    i++;
+  }
+}
+
+void recieve_string(char string_recieved[], uint8_t max_string_length) {
+  char byte_recieved;
+  uint8_t i = 0;
+
+  while(i < max_string_length - 1){
+    byte_recieved = recieve_byte();
+    if(response == "\r"){
+      break;
+    } else {
+      string_recieved[i] = byte_recieved;
+      i++;
+    }
+  }
+
+  string_recieved[i] = 0;
 }
--- libs/UART.h
+++ libs/UART.h
@@ -2,3 +2,7 @@
 #define BAUD_RATE ((F_CPU)/(BAUD*16UL)-1)
 
 void uart_init(void);
+void send_byte(uint8_t data);
+uint8_t recieve_byte(void);
+void print_string(const char string_to_send);
+void recieve_string(char string_recieved[], uint8_t max_string_length);
