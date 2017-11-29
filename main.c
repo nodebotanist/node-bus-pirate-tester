448f9f982644bb61ad5dc43ada9c74c590986044
Kas Perch <the@nodebotani.st>
2017-11-28 22:51:06 -0600

    Added start prompt for UART

--- main.c
+++ main.c
@@ -4,6 +4,10 @@
 #include "UART.h"
 
 void setup(){
+  uart_init();
+  uart_print_string("Welcome to the Bus Pirate Testing Peripheral");
+  uart_print_string("-----------");
+  uart_print_string("Attach the Bus Pirate to the Peripheral and run the associated suite of tosts as shown in the project README.md");
 }
 
 int main(){
