a2bb0af0c900ade2c241b1541f0402ad80814d48
Kas Perch <the@nodebotani.st>
2017-12-20 16:26:08 -0600

    I2C lib implemented

--- libs/I2C.c
+++ libs/I2C.c
@@ -19,25 +19,101 @@ void I2C_main_start(void){
 }
 
 void I2C_main_stop(void){
-
+  TWCR = (_BV(TWINT) | _BV(TWEN) | _BV(TWSTO));
 }
 
 uint8_t I2C_main_read(void){
-  return 0;
+  TWCR = (_BV(TWINT) | _BV(TWEN) | _BV(TWEA));
+  I2C_wait_for_complete();
+  return (TWDR);
 }
 
 void I2C_send(uint8_t data){
-
+  TWDR = data;
+  TWCR = (_BV(TWINT) | _BV(TWEN));
+  I2C_wait_for_complete();
 }
 
 void I2C_peripheral_start(void){
-
+  // set I2C peripheral address
+  TWAR = (address << 1);
+  // Enable address matching
+  // Enable I2C
+  // Clear I2C interrupt
+  // Enable I2C interrupt on data recieved
+  TWCR = (_BV(TWIE) | _BV(TWEA) | _BV(TWINT) | _BV(TWEN));
 }
 
 void I2C_peripheral_stop(void){
-
+  // Clear ACK and enable bits
+  TWCR &= ~(_BV(TWEA) | _BV(TWEN));
 }
 
 ISR(TWI_vect){
+  // temporarily store data
+  uint8_t data;
 
+  bool recieved = (TWSR & 0xF8);
+
+  if(recieved == TW_SR_SLA_ACK) {
+    // byte recieved was address of peripheral
+    // prepare to recieve buffer address
+    buffer_address = 0xFF;
+    I2C_peripheral_ACK();
+  } else if (recieved == TW_SR_DATA_ACK) {
+    // data byte recieved by peripheral
+    data = TWDR;
+
+    if(buffer_address == 0xFF) {
+      // first byte rceieved is buffer address
+      buffer_address = data;
+      I2C_peripheral_ACK();
+    } else {
+      input_buffer[buffer_address] = data;
+      buffer_address++;
+
+      if(buffer_address < 0xFF) {
+        // If there's room left in the buffer
+        I2C_peripheral_ACK();
+      } else {
+        I2C_peripheral_NACK();
+      }
+    }
+  } else if (recieved == TW_ST_DATA_ACK) {
+    // data requested from peripheral
+    data = TWDR;
+    
+    if(buffer_address == 0xFF) {
+      // read adress not sent yet, byte is read address
+      buffer_address = data;
+    }
+
+    // copy output buffer for transmission
+    TWDR = output_buffer[buffer_address];
+    buffer_address++;
+
+    if(buffer_address < 0xFF) {
+      I2C_peripheral_ACK();
+    } else {
+      I2C_peripheral_NACK();
+    }
+  } else {
+    // prepare peripheraal to be addressed again
+    TWCR |= (1<<TWIE) | (1<<TWEA) | (1<<TWEN);
+  }
+}
+
+void I2C_peripheral_ACK(void) {
+    // clear interrupt
+    // prepare for next byte
+    // ACK
+    TWCR |= (_BV(TWIE) | _BV(TWINT) | _BV(TWEN) | _BV(TWEA));
+}
+
+void I2C_peripheral_NACK(void) {
+  // NACK
+  TWCR &= ~(_BV(TWEA));
+  // clear interrupt
+  // prepare for next byte
+  TWCR |= (_BV(TWIE) | _BV(TWINT) | _BV(TWEN));
 }
--- libs/I2C.h
+++ libs/I2C.h
@@ -1,18 +1,23 @@
 // I2C library
 
+// buffer for address, input, output
+volatile uint8_t buffer_address = 0xFF;
+volatile uint8_t input_buffer[0x00];
+volatile uint8_t output_buffer[0x00];
+
 // I2C_Init -- configures AVR as I2C main device multiplier can be set to 1 (100 kHz), or 4 (400 kHz)
 void I2C_init(uint8_t multiplier);
 
 // I2C_wait_for_complete -- returns when TWINT bit is set
 void I2C_wait_for_complete(void);
 
-// I2C_start -- starts I2C comms
+// I2C_main_start -- starts I2C comms
 void I2C_main_start(void);
 
-// I2C_stop -- stops I2C comms
+// I2C_main_stop -- stops I2C comms
 void I2C_main_stop(void);
 
-// I2C_read -- reads and waits for ACK
+// I2C_main_read -- reads and waits for ACK
 uint8_t I2C_main_read(void);
 
 // I2C_send -- sends a byte via I2C
@@ -29,3 +34,9 @@ void I2C_peripheral_stop(void);
 
 // Interrupt for I2C peripheral recieve
 ISR(TWI_vect);
+
+// I2C_peripheral_ACK() -- sends ACK and clears interrupt
+void I2C_peripheral_ACK(void);
+
+// I2C_peripheral_NACK() -- sends NACK and clears interrupt
+void I2C_peripheral_NACK(void);
