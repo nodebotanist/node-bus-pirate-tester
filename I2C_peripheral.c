192ee1e204dda931a43b774cc82fb569af3cd4fc
Kas Perch <the@nodebotani.st>
2017-12-20 18:22:00 -0600

    added main()

--- I2C_peripheral.c
+++ I2C_peripheral.c
@@ -3,10 +3,13 @@
 #include <util/twi.h>
 #include <avr/interrupt.h>
 #include "I2C.h"
-#include "UART.h"
 
 void setup() {
   uint8_t address = 0x3F;
-  I2C_init();
+  I2C_init(4);
   I2C_peripheral_start(address);
+}
+
+void main() {
+  
 }
