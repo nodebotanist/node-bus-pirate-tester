d6c13f18d7d4f2e1f1511ddf1885ca82eb36e7d4
Mx. Kas Perch <the@nodebotani.st>
2018-02-21 23:15:50 -0600

    Scaffolding I2C functions

--- I2C_peripheral.c
+++ I2C_peripheral.c
@@ -4,10 +4,25 @@
 #include <avr/interrupt.h>
 #include "I2C.h"
 
+uint8_t write_reg_value = 0x00
+uint8_t read_reg_value[2] = [0x12, 0x34]
+
 void setup() {
   uint8_t address = 0x3F;
   I2C_init(4);
   I2C_peripheral_start(address);
+}
+
+void read() {
+
+}
+
+void write() {
+
+}
+
+void read_from_write() {
+  
 }
 
 void main() {
